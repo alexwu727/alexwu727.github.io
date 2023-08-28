476778b95fdac6d6fdfded0f1df7ca50386ebd1c
Alex Wu <alexwu727@gmail.com>
2023-08-28 09:18:22 -0700

    changed the order

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <NavLink to="/about" className='navItem'> About me </NavLink>
-            <NavLink to="/projects" className='navItem'> Projects </NavLink>
             <NavLink to="/experiences" className='navItem'> Experiences </NavLink>
+            <NavLink to="/projects" className='navItem'> Projects </NavLink>
             <NavLink to="/contact" className='navItem'> Contact </NavLink>
         </div>
     )
