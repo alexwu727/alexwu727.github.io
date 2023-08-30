81ab50cd47eab8a175b49b607a29f3bb1e7b0344
Alex Wu <alexwu727@gmail.com>
2023-08-30 16:36:55 -0700

    Modify an item name.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <NavLink to="/about" className='navItem'> About me </NavLink>
-            <NavLink to="/experiences" className='navItem'> Experiences </NavLink>
+            <NavLink to="/experiences" className='navItem'> Experience </NavLink>
             <NavLink to="/projects" className='navItem'> Projects </NavLink>
             <NavLink to="/contact" className='navItem'> Contact </NavLink>
         </div>
